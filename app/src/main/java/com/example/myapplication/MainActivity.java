e56fa05ac34548c27d2f84edc4e9efef2e93ccf7
salahaddin <salahaddin.personal@gmail.com>
2021-09-06 11:05:50 +0200

    test ssh connection

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
     }
 
